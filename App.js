25a84e27b51e181134a7a57efb645ddf01d28669
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2018-09-06 09:51:51 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Item, Label, Input, Container} from 'native-base';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+        <Container style={{paddingTop: 70}}>
+          <Item floatingLabel>
+              <Label>This does not appear in release mode</Label>
+              <Input/>
+          </Item>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ README.md
@@ -0,0 +1,13 @@
+Minimal example to demonstate problem with floating labels in native-base 2.8.0
+==
+
+
+In dev mode
+--
+![Developer mode](dev.png)
+
+
+In release mode
+--
+![Release mode](release.png)
+
